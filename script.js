61a26d3d1a55d5d18c3e54766aae872bd6426fae
Camillacristinarj <camillarj86@gmail.com>
2026-05-04 21:18:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+body {
+  font-family: Arial;
+  background: linear-gradient(to right, #667eea, #764ba2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.container {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+  width: 300px;
+  text-align: center;
+}
+
+input {
+  padding: 10px;
+  width: 70%;
+}
+
+button {
+  padding: 10px;
+  cursor: pointer;
+  border: none;
+  background: #667eea;
+  color: white;
+  border-radius: 5px;
+}
+
+li {
+  list-style: none;
+  margin-top: 10px;
+  background: #eee;
+  padding: 10px;
+  border-radius: 5px;
+}
